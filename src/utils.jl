1bf750efe61d6f47191862019ec623a82b0f4ea3
hdunham <70401017+hdunham@users.noreply.github.com>
2022-06-10 21:19:26 -0600

    round tech sizes in summary csv

--- src/utils.jl
+++ src/utils.jl
@@ -104,9 +104,9 @@ function summarize_results(; sites, warming_plus_deg_C)
             df_results_summary = hcat(df_results_summary, DataFrame(
                     "+$(plus_deg)C" => [
                         round(results["Thermosyphon"]["min_annual_active_cooling_mmbtu"], digits=3),
-                        results["PV"]["size_kw"]*1000,
-                        results["ElectricStorage"]["size_kw"]*1000,
-                        results["ElectricStorage"]["size_kwh"]*1000,
+                        round(results["PV"]["size_kw"]*1000, digits=1),
+                        round(results["ElectricStorage"]["size_kw"]*1000, digits=1),
+                        round(results["ElectricStorage"]["size_kwh"]*1000, digits=1),
                         round(count(i -> (i > 0), results["Thermosyphon"]["active_cooling_series_btu_per_hour"]) * 100 / 8760, digits=2),
                         if typeof(results["optimality_gap"])<:Real round(results["optimality_gap"]*100, digits=2) else results["optimality_gap"] end
                     ]
